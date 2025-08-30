626d8841ca2eecddbc4d5c2d3ea9f4f521a56c00
william.plancke <william.plancke@student.howest.be>
2025-08-30 13:04:30 +0200

    Add more routes for future use

--- routes/api.php
+++ routes/api.php
@@ -7,3 +7,6 @@ use App\Http\Controllers\Controller;
 Route::get('/clients', [Controller::class, 'getClients']);
 Route::get('/lawyers', [Controller::class, 'getLawyers']);
 Route::post('/appointments', [Controller::class, 'setAppointment']);
+Route::delete('/appointments', [Controller::class, 'deleteAllAppointments']);
+Route::delete('/appointments/{appointmentId}', [Controller::class, 'deleteAppointment']);
+Route::delete('/clients/{clientId}',[Controller::class, 'deleteClient']);
